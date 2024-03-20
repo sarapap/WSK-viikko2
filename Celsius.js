0bc0f15362e156055629d5da051f482d605616bd
sarapappila <sara.pappila@metropolia.fi>
2024-03-20 10:48:18 +0200

    New commit

---
+++ Celsius.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const temp = prompt('Enter a temperature in Celsius.');
+const convertedF = temp * (9/5) + 32;
+const convertedK = temp + 274.15;
+
+document.querySelector('#fahrenheit').innerHTML = 'Converted value in Fahrenheit: ' + parseFloat(convertedF.toFixed(2));
+document.querySelector('#kelvin').innerHTML = 'Converted value in Kelvin: ' + parseFloat(convertedK.toFixed(2));
